a9b1b354c83b53a114cac227c9664774a45089b8
Duy Anh <duyanh.lekhac@hds-international.vn>
2024-12-12 18:20:57 +0700

    make it clean

--- content.js
+++ content.js
@@ -1,18 +1,23 @@
+var IsTextNode = node => node.nodeType === 3
+var Span = (html) => {
+  const span = document.createElement('span');
+  span.innerHTML = html 
+  return span
+}
 
 chrome.storage.sync.get('keywords', ({ keywords }) => {
   if (!keywords || keywords.length === 0) return;
   const regex = new RegExp(`(${keywords.join('|')})`, 'gi');
   const walk = (node) => {
-    if (node.nodeType === 3) {
-      const matches = node.nodeValue.match(regex);
-      if (matches) {
-        const span = document.createElement('span');
-        span.innerHTML = node.nodeValue.replace(regex, (match) => `<mark>${match}</mark>`);
-        node.replaceWith(span);
-      }
-    } else {
-      node.childNodes.forEach(walk);
-    }
+    if (!IsTextNode(node)) node.childNodes.forEach(walk)
+    else {
+      const found = node.nodeValue.match(regex);
+      if (found) {
+        let span = Span(node.nodeValue.replace(regex, (match) => `<mark>${match}</mark>`))
+        node.replaceWith(span)
+      };
+    };
   };
+
   walk(document.body);
 });
