c62fddd26ec309e7838a439c448379b21dafccd7
Duy Anh <duyanh.lekhac@hds-international.vn>
2024-12-13 18:49:40 +0700

    refactor the shit

--- hili.js
+++ hili.js
@@ -1,9 +1,9 @@
 var IsTextNode = node => node.nodeType === 3
-var IsScriptTag = node => node.parentNode.nodeName === 'SCRIPT'
-var IsNoScriptTag = node => node.parentNode.nodeName === 'NOSCRIPT'
-var IsStyleTag = node => node.parentNode.nodeName === 'STYLE'
-var IsFormTag = node => node.parentNode.nodeName === 'FORM'
-var IsMarked = node => node.parentNode.nodeName === 'MARK'
+var IsScriptTag = node => node.parentNode?.nodeName === 'SCRIPT'
+var IsNoScriptTag = node => node.parentNode?.nodeName === 'NOSCRIPT'
+var IsStyleTag = node => node.parentNode?.nodeName === 'STYLE'
+var IsFormTag = node => node.parentNode?.nodeName === 'FORM'
+var IsMarked = node => node.parentNode?.nodeName === 'MARK'
 
 var IsNodeValid = node => [IsScriptTag, IsNoScriptTag, IsStyleTag, IsFormTag, IsMarked].every(test => !test(node))
 
@@ -13,22 +13,31 @@ var Span = (html) => {
   return span
 }
 
-function hili() {
-  chrome.storage.sync.get('keywords', ({ keywords }) => {
-    if (!keywords || keywords.length === 0) return;
-    const regex = new RegExp(`(${keywords.join('|')})`, 'gi');
-    const walk = (node) => {
+var Walker = ({ matcher, replacer }) => {
+  const walk = (node) => {
       if (!IsTextNode(node)) node.childNodes.forEach(walk)
       else if (IsNodeValid(node)) {
-        const found = node.nodeValue.match(regex)
+        const found = matcher(node)
         if (found) {
-          let span = Span(node.nodeValue.replace(regex, (match) => `<mark>${match}</mark>`))
-          node.replaceWith(span)
+          node.replaceWith(Span(replacer(node, `<mark>${found}</mark>`)))
         };
       };
     };
 
-    walk(document.body);
+  return {
+    walk
+  }
+}
+
+function hili(node) {
+  chrome.storage.sync.get('keywords', ({ keywords }) => {
+    if (keywords?.length === 0) return;
+    const regex = new RegExp(`(${keywords.join('|')})`, 'gi');
+    const replacer = (node, replacement) => node.nodeValue.replace(regex, replacement)
+    const matcher = node => node.nodeValue.match(regex) 
+    const walker = Walker({ matcher, replacer }) 
+
+    walker.walk(node);
   });
 }
 
@@ -36,12 +45,16 @@ function onDomChanged(targetNode, callback) {
   const config = { attributes: false, childList: true, subtree: true };
   const observer = new MutationObserver((mutationList, observer) => {
     for (const mutation of mutationList) {
-      if (mutation.type === "childList") callback()
+      if (mutation.type === "childList") {
+        for (const node of mutation.addedNodes) {
+          callback(node)
+        }
+      }
     }
   });
   observer.observe(targetNode, config);
 }
 
-hili()
-onDomChanged(document.body, () => hili())
+hili(document.body)
+onDomChanged(document.body, (node) => hili(node))
 
